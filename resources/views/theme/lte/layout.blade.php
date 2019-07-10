d3ae52b8193764f2a77ff9564dfe785a308efb83
Curso de Laravel <jtallon54@yahoo.es>
2019-07-10 14:30:45 +0200

    Menus en contruccion

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -17,8 +17,9 @@
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="{{asset("assets/$theme/dist/css/skins/_all-skins.min.css")}}">
-  <link rel="stylesheet" href="{{asset("assets/css/custom.css")}}">
   @yield('styles')
+  <link rel="stylesheet" href="{{asset("assets/css/custom.css")}}">
+  
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -62,6 +63,14 @@
 <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
 <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
 <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+<script src="{{asset("assets/js/funciones.js")}}"></script>
+
+@yield("scriptsPlugins")
+    <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
+    <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="{{asset("assets/js/scripts.js")}}"></script>
 <script src="{{asset("assets/js/funciones.js")}}"></script>
 @yield('script')
 </body>
--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,21 @@ Route::group(['prefix' => 'admin', 'namespace'=>'Admin'], function () {
     Route::get('permiso/crear', 'PermisoController1@crear')->name('crear_permiso');
     Route::get('menu/crear', 'MenuController@crear')->name('crear_menu');
     Route::post('menu', 'MenuController@guardar')->name('guardar_menu');
+
+     /*RUTAS DEL MENU*/
+     Route::get('menu', 'MenuController@index')->name('menu');
+     Route::get('menu/crear', 'MenuController@crear')->name('crear_menu');
+     Route::post('menu', 'MenuController@guardar')->name('guardar_menu');
+     Route::post('menu/guardar-orden', 'MenuController@guardarOrden')->name('guardar_orden');
+     /*RUTAS ROL*/
+     Route::get('rol', 'RolController@index')->name('rol');
+     Route::get('rol/crear', 'RolController@crear')->name('crear_rol');
+     Route::post('rol', 'RolController@guardar')->name('guardar_rol');
+     Route::get('rol/{id}/editar', 'RolController@editar')->name('editar_rol');
+     Route::put('rol/{id}', 'RolController@actualizar')->name('actualizar_rol');
+     Route::delete('rol/{id}', 'RolController@eliminar')->name('eliminar_rol');
+     /*RUTAS MENU_ROL*/
+     Route::get('menu-rol', 'MenuRolController@index')->name('menu_rol');
+     Route::post('menu-rol', 'MenuRolController@guardar')->name('guardar_menu_rol');
 });
 
